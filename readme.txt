fca6518a9c516459b17ea548b99d71e733248640
storeya <storeya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 21:06:21 +0000

    initial version
    git-svn-id: https://plugins.svn.wordpress.org/storeya-like-box/trunk@1088478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,69 @@
+﻿=== StoreYa Like Box ===
+Contributors: StoreYa
+Tags: Facebook, Marketing tools, Social commerce, storeya, fans, add fans, plugin, sharing, network, facebook likes, Like box
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Like Box plugin increasing your Facebook Community from day one! 
+
+== Description ==
+
+**Exposed to 1,000,000 customers daily, the Like Box results speak for themselves:**
+
+- Grow your Facebook community easily 
+
+– Drive more traffic from Facebook to your web store daily 
+
+– Boost your sales by making random visitors into fans, growing your community and promoting your offers on Facebook 
+
+– Create an increased "Word-of-Mouth" effect. 
+
+The Like Box allows you to promote your social marketing by creating a customized "like box" and converting customers into engaged fans. 
+You can select whether to display thumbnails of your customers' friends who have already become fans of your business, or show the latest posts shared on your Facebook fan page.
+
+To learn more about StoreYa and how it works visit [our website]( http://www.storeya.com/public/likebox#)
+
+== Installation ==
+
+1. Download the  zip file and save it locally - 'storeya-like-box.zip'.
+
+2. Log in to your WordPress administration screen.
+
+3. Click on 'Plugins' and then on 'Add New' (left menu).
+
+4. Click on the 'Upload' link, choose 'storeya-like-box.zip' file and click on the 'Install' now button.
+
+5. Click on 'Activate Plugin' link
+
+6. Click on the StoreYa Like Box > Settings
+
+8. Go to www.storeya.com/public/likebox
+
+9. If you are not logged in, please click on the "Create a Free Account!" button.
+
+10. Once you are happy with your Like Box customization, copy the code and paste it to the Like Box for WP setting screen. 
+
+11. Click 'Save changes'.
+
+== Screenshots ==
+
+1. Example 1
+
+2. Example 2
+
+3. Example 3
+
+4. Like Box Settings
+
+5. Example 5
+
+
+
+== Resources ==
+
+[StoreYa support](https://storeya.zendesk.com/home) 
+
+[Like Box website](http://www.storeya.com/public/likebox)
---
+++ storeya-like-box.php
@@ -0,0 +1,161 @@
+<?php
+/*
+Plugin Name: StoreYa Like Box
+Plugin URI: http://www.storeya.com/public/likebox
+Description: Like Box plugin increasing your Facebook Community from day one! 
+Version: 1.0
+Author: StoreYa
+Author URI: http://www.storeya.com/
+
+=== VERSION HISTORY ===
+02.02.15 - v1.0 - The first version
+
+=== LEGAL INFORMATION ===
+Copyright © 2013 StoreYa Feed LTD - http://www.storeya.com/
+
+License: GPLv2 
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+add_action('wp_footer', 'slb_insert');
+
+function slb_insert()
+{
+    global $current_user;
+    if (get_option('slbID')) {
+            
+        $secret_key = get_option('slbID');
+	    $sty_script = "<script>(function(e){ var t = e.createElement(\"script\"); t.src = \"//www.storeya.com/externalscript/storeyaall?sid=".$secret_key."\"; t.type = \"text/javascript\"; t.async = true;e.getElementsByTagName(\"head\")[0].appendChild(t)})(document)</script>";
+	
+	$script = str_replace("\"","\"",$sty_script);
+        echo $script; 
+    }
+}
+
+
+if ( is_admin() ) {
+	
+
+
+$plugurldir = get_option('siteurl') . '/' . PLUGINDIR . '/storeya-like-box/';
+$slb_domain = 'StoreYaLikeBox';
+load_plugin_textdomain($slb_domain, false, dirname( plugin_basename( __FILE__ ) ) . '/storeya-like-box/');
+add_action('init', 'slb_init');
+
+add_action('admin_notices', 'slb_admin_notice');
+add_filter('plugin_action_links', 'slb_plugin_actions', 10, 2);
+
+
+
+function slb_init()
+{
+    if (function_exists('current_user_can') && current_user_can('manage_options'))
+        add_action('admin_menu', 'slb_add_settings_page');
+    if (!function_exists('get_plugins'))
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    $options = get_option('slbDisable');
+}
+function slb_settings()
+{
+    register_setting('storeya-like-box-group', 'slbID');
+    register_setting('storeya-like-box-group', 'slbDisable');
+    add_settings_section('storeya-like-box', "StoreYa Like Box", "", 'storeya-like-box-group');
+
+}
+function slb_plugin_get_version()
+{
+    if (!function_exists('get_plugins'))
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    $plugin_folder = get_plugins('/' . plugin_basename(dirname(__FILE__)));
+    $plugin_file   = basename((__FILE__));
+    return $plugin_folder[$plugin_file]['Version'];
+}
+
+
+function slb_admin_notice()
+{
+    if (!get_option('slbID'))
+        echo ('<div class="error"><p><strong>' . sprintf(__('StoreYa Like Box  is disabled. Please go to the <a href="%s">plugin page</a> and enter a valid Like Box script to enable it.'), admin_url('options-general.php?page=storeya-like-box')) . '</strong></p></div>');
+}
+function slb_plugin_actions($links, $file)
+{
+    static $this_plugin;
+    if (!$this_plugin)
+        $this_plugin = plugin_basename(__FILE__);
+    if ($file == $this_plugin && function_exists('admin_url')) {
+        $settings_link = '<a href="' . admin_url('options-general.php?page=storeya-like-box') . '">' . __('Settings', $slb_domain) . '</a>';
+        array_unshift($links, $settings_link);
+    }
+    return ($links);
+}
+
+    function slb_add_settings_page()
+    {
+        function slb_settings_page()
+        {
+            global $slb_domain, $plugurldir, $storeya_options;
+?>
+      <div class="wrap">
+        <?php
+            screen_icon();
+?>
+        <h2><?php
+            _e('StoreYa Like Box ', $slb_domain);
+?> <small><?
+            echo slb_plugin_get_version();
+?></small></h2>
+        <div class="metabox-holder meta-box-sortables ui-sortable pointer">
+          <div class="postbox" style="float:left;width:30em;margin-right:20px">
+            <h3 class="hndle"><span><?php
+            _e('StoreYa Like Box  - Settings', $slb_domain);
+?></span></h3>
+            <div class="inside" style="padding: 0 10px">
+            <p style="text-align:center"><a href="http://www.storeya.com/public/likebox" target="_blank" title="<?php
+            _e('Convert your visitors to paying customers with StoreYa!', $srff_domain);
+?>"><img src="<?php
+            echo (plugins_url( 'storeya-like-box.jpg', __FILE__ ));
+?>" height="200" width="200" alt="<?php
+            _e('StoreYa Logo', $srff_domain);
+?>" /></a></p>
+              <form method="post" action="options.php">
+                <?php
+            settings_fields('storeya-like-box-group');
+?>
+                <p><label for="slbID"><?php
+            printf(__('
+Enter Like Box secret key you got from %1$sIncrease your online sales today with StoreYa!%2$sStoreYa%3$s.', $slb_domain), '<strong><a href="http://www.storeya.com/public/likebox" target="_blank"  title="', '">', '</a></strong>');
+?></label></p>
+
+                  <p><textarea rows="1" cols="20" name="slbID" ><?php echo get_option('slbID');?></textarea></p>
+                    <p class="submit">
+                      <input type="submit" class="button-primary" value="<?php
+            _e('Save Changes');
+?>" />
+                    </p>
+                  </form>
+</p>
+                  <p style="font-size:smaller;color:#999239;background-color:#ffffe0;padding:0.4em 0.6em !important;border:1px solid #e6db55;-moz-border-radius:3px;-khtml-border-radius:3px;-webkit-border-radius:3px;border-radius:3px"><?php
+            printf(__('Don&rsquo;t have a Like Box? No problem! %1$sKeep your visitors engaged with you in all social networks you are active on!%2$sCreate a <strong>FREE</strong> StoreYa Like Box  Now!%3$s', $slb_domain), '<a href="http://www.storeya.com/public/likebox" target="_blank" title="', '">', '</a>');
+?></p>
+                  </div>
+                </div>
+
+                </div>
+              </div>
+			  <img src="http://www.storeya.com/widgets/admin?p=WpLikeBox"/>
+              <?php
+        }
+        add_action('admin_init', 'slb_settings');
+        add_submenu_page('options-general.php', __('StoreYa Like Box ', $slb_domain), __('StoreYa Like Box ', $slb_domain), 'manage_options', 'storeya-like-box', 'slb_settings_page');
+    }
+    
+    } else {
+    if ( isset ( $_REQUEST['action'] ) && 'storeya_sync_data' == $_REQUEST['action'] ) {
+	    require_once ( 'SyncManager.php' );
+	     require_once ( 'StoreYaAPI.php' );
+	     }
+}
+
+
+
+?>
